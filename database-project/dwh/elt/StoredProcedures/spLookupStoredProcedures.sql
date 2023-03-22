d86c274acf258a858357fe35ed8dfa1a17fa440b
Sjors van der Geest <sjors.vandergeest@vanoord.com>
2023-03-22 14:56:56 +0100

    sp + logic

--- database-project/dwh/elt/StoredProcedures/spLookupStoredProcedures.sql
+++ database-project/dwh/elt/StoredProcedures/spLookupStoredProcedures.sql
@@ -3,7 +3,7 @@
 AS
 BEGIN
     
-    SELECT CONCAT('[',[Schema]/*1*/,'].[Load_'/*2*/, [EntityName], ']') as [Procedure] --*1 Project is het schema van de storage-laag. *2 De stored procedures dienen als voorzetsel 'Load_' in de naam te hebben.
+    SELECT CONCAT('[',[Schema],'].[', [EntityName], ']') as [Procedure]
 	 ,[Schema]
 	 ,[EntityName]
 	FROM [elt].[StorageTables] 
