86c6784f7607ac2786d18d15223b0c8bf33d5c5a
Axel van 't Westeinde <axel.vantwesteinde@vanoord.com>
2023-05-03 13:08:34 +0200

    updated names of servicenow stored procedures to adhere to naming convention

---
+++ database-project/mdb/elt/StoredProcedures/spSnInsertParentTables.sql
@@ -0,0 +1,16 @@
+CREATE PROCEDURE [elt].[SnInsertParentTables]
+    @parent_tables nvarchar(255),
+	@base_table nvarchar(255),
+    @system_name nvarchar(255),
+    @system_code nvarchar(255)
+AS   
+IF EXISTS (SELECT * FROM elt.MetadataTables where EntityName = @base_table AND SystemCode = @system_code AND SystemName = @system_name)
+BEGIN
+UPDATE [elt].[MetadataTables] SET ParentTable = @parent_tables WHERE EntityName = @base_table AND SystemCode = @system_code AND SystemName = @system_name
+END
+ELSE
+BEGIN
+INSERT INTO [elt].[MetadataTables](EntityName, SystemCode, SystemName, ParentTable) VALUES (@base_table, @system_code, @system_name, @parent_tables)
+END
+GO
+
