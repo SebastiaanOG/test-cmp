4fb1dc4f402388ed18020b8d8a234c686706118d
Mohammed <114924143+MohammedSZN@users.noreply.github.com>
2023-05-04 12:47:11 +0200

    add MetadataTable records for ServiceNow Projects

--- database-project/mdb/PostDeploy/PostDeploymentScript.sql
+++ database-project/mdb/PostDeploy/PostDeploymentScript.sql
@@ -322,3 +322,17 @@ INSERT [elt].[TypeMap] ([SourceDataType], [InterimDataType], [SystemType], [Sink
 INSERT [elt].[TypeMap] ([SourceDataType], [InterimDataType], [SystemType], [SinkDataType]) VALUES (N'condition_string',N'string',N'servicenow',N'nvarchar')
 
 GO
+
+/*Insert MetadataTable records for ServiceNow Projects */
+-- Includes values for columns which are NOT NULL and IncrementColumnName
+INSERT INTO [elt].[MetadataTables] (SystemCode, SystemName, SchemaName, EntityName, CopyToStg, CopyToRaw, IncrementColumnName)
+VALUES 
+('snprojects', 'servicenow-projects', 'api', 'dmn_demand', 1, 0, 'sys_updated_on'),
+('snprojects', 'servicenow-projects', 'api', 'issue', 1, 0, 'sys_updated_on'),
+('snprojects', 'servicenow-projects', 'api', 'pm_portfolio', 1, 0, 'sys_updated_on'),
+('snprojects', 'servicenow-projects', 'api', 'pm_project', 1, 0, 'sys_updated_on'),
+('snprojects', 'servicenow-projects', 'api', 'pm_project_task', 1, 0, 'sys_updated_on'),
+('snprojects', 'servicenow-projects', 'api', 'project_status', 1, 0, 'sys_updated_on'),
+('snprojects', 'servicenow-projects', 'api', 'risk', 1, 0, 'sys_updated_on');
+
+GO
