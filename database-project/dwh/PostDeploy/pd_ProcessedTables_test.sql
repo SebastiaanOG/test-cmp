ba74fe4efd0f52011338e8f8c535348a9cf7aa66
Sebastiaan <Sebastiaan@mail.com>
2023-06-13 16:12:32 +0200

    changes test script

--- database-project/dwh/PostDeploy/pd_ProcessedTables_test.sql
+++ database-project/dwh/PostDeploy/pd_ProcessedTables_test.sql
@@ -1,8 +1,8 @@
 -- This file contains SQL statements that will be executed after the build script.
 
 /* Truncate before insert */
-TRUNCATE TABLE [elt].[ProcessedTables]
+--TRUNCATE TABLE [elt].[ProcessedTables]
 
 /*Insert records to pickup in the processed layer */
-INSERT [elt].[ProcessedTables] ([Level], [EntityName], [UseCaseCode], [StoredProcedureName], [Schema], [Active], [Comment]) VALUES (N'1', N'fuel_prices', N'cmptest', N'cmptest', N'processed', N'1', N'This dataset is a dummy set to test end to end logic of the framework')
+INSERT [elt].[ProcessedTables] ([Level], [EntityName], [UseCaseCode], [StoredProcedureName], [Schema], [Active], [Comment]) VALUES (N'2', N'fuel_prices', N'cmptest', N'cmptest', N'processed', N'1', N'This dataset is a dummy set to test end to end logic of the framework')
 
