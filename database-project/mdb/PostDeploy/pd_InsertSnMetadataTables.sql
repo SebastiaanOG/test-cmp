19fe2b9efea77c7233c992a1babf03f31b9757e4
Mohammed <114924143+MohammedSZN@users.noreply.github.com>
2023-06-14 14:07:53 +0200

    remove _increment for service now projects

--- database-project/mdb/PostDeploy/pd_InsertSnMetadataTables.sql
+++ database-project/mdb/PostDeploy/pd_InsertSnMetadataTables.sql
@@ -2,14 +2,14 @@
 DELETE FROM [elt].[MetadataTables]
 WHERE SystemCode = 'snp'
 -- Includes values for columns which are NOT NULL and IncrementColumnName
-INSERT INTO [elt].[MetadataTables] (SystemCode, SystemName, SchemaName, EntityName, IncrementColumnName)
+INSERT INTO [elt].[MetadataTables] (SystemCode, SystemName, SchemaName, EntityName)
 VALUES 
-('snp', 'servicenow-projects', 'api', 'dmn_demand', 'sys_updated_on'),
-('snp', 'servicenow-projects', 'api', 'issue', 'sys_updated_on'),
-('snp', 'servicenow-projects', 'api', 'pm_portfolio', 'sys_updated_on'),
-('snp', 'servicenow-projects', 'api', 'pm_project', 'sys_updated_on'),
-('snp', 'servicenow-projects', 'api', 'pm_project_task', 'sys_updated_on'),
-('snp', 'servicenow-projects', 'api', 'project_status', 'sys_updated_on'),
-('snp', 'servicenow-projects', 'api', 'risk', 'sys_updated_on');
+('snp', 'servicenow-projects', 'api', 'dmn_demand'),
+('snp', 'servicenow-projects', 'api', 'issue'),
+('snp', 'servicenow-projects', 'api', 'pm_portfolio'),
+('snp', 'servicenow-projects', 'api', 'pm_project'),
+('snp', 'servicenow-projects', 'api', 'pm_project_task'),
+('snp', 'servicenow-projects', 'api', 'project_status'),
+('snp', 'servicenow-projects', 'api', 'risk');
 
 GO
