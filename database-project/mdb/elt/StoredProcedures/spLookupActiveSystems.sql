d03dcb63fa17b5dc003aa58a31941656408d02a7
sebastiaanvanoord <121857265+sebastiaanvanoord@users.noreply.github.com>
2023-07-31 16:39:00 +0200

    Feature/mdb system code switch (#144)
    * add systemcode to stored procedure
    
    * Updating pipeline: 00 MDB
    
    * Updating pipeline: Master
    
    * Updating pipeline: 00 MDB
    
    ---------
    
    Co-authored-by: Sebastiaan <Sebastiaan@mail.com>

--- database-project/mdb/elt/StoredProcedures/spLookupActiveSystems.sql
+++ database-project/mdb/elt/StoredProcedures/spLookupActiveSystems.sql
@@ -1,5 +1,6 @@
-CREATE PROCEDURE [elt].[spLookupActiveSystems] 
+CREATE PROCEDURE [elt].[spLookupActiveSystems] (@SystemCode	nvarchar(128))
 AS
 SELECT DISTINCT ms.*
 FROM [elt].[MetadataSystem] ms
 WHERE ms.Active = 1
+AND ms.SystemCode = @SystemCode
